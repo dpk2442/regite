79a6b316648e163283ed7e18e25fb65b0bb540c9
Dave Korhumel <dpk2442@gmail.com>
2022-04-03 23:09:45 -0700

    add integration test and fix metric formatting

--- src/metric.rs
+++ src/metric.rs
@@ -27,17 +27,12 @@ pub trait MetricReporter: Send {
 }
 
 struct MetricReporterImpl {
-    prefix: String,
-    hostname: String,
     send_fn: Box<dyn Fn(&str) -> io::Result<()> + Send>,
 }
 
 impl MetricReporter for MetricReporterImpl {
     fn report(&self, name: &str, value: &str, timestamp: u64) -> Result<(), MetricReporterError> {
-        let metric = format!(
-            "{}.{}.{} {} {}\n",
-            self.prefix, self.hostname, name, value, timestamp
-        );
+        let metric = format!("{} {} {}\n", name, value, timestamp);
         match (self.send_fn)(&metric) {
             Ok(()) => Ok(()),
             Err(e) => Err(MetricReporterError::new(format!("IO Error: {}", e))),
@@ -49,8 +44,6 @@ pub fn build(config: &config::General) -> Box<dyn MetricReporter> {
     let socket = net::UdpSocket::bind("[::]:0").expect("Unable to bind to ephemeral port");
     let address = config.graphite_address.clone();
     Box::new(MetricReporterImpl {
-        prefix: config.prefix.to_string(),
-        hostname: config.hostname.to_string(),
         send_fn: Box::new(move |s| socket.send_to(s.as_bytes(), &address).and(Ok(()))),
     })
 }
@@ -62,10 +55,8 @@ mod test {
     #[test]
     fn test_formats() {
         let reporter = MetricReporterImpl {
-            prefix: "prefix".to_string(),
-            hostname: "hostname".to_string(),
             send_fn: Box::new(|s| {
-                assert_eq!("prefix.hostname.name value 123\n", s);
+                assert_eq!("name value 123\n", s);
                 Ok(())
             }),
         };
@@ -76,8 +67,6 @@ mod test {
     #[test]
     fn test_io_error() {
         let reporter = MetricReporterImpl {
-            prefix: "prefix".to_string(),
-            hostname: "hostname".to_string(),
             send_fn: Box::new(|_| Err(io::Error::from(io::ErrorKind::Unsupported))),
         };
 
---
+++ tests/integration.rs
@@ -0,0 +1,47 @@
+#![warn(clippy::all)]
+#![cfg(target_os = "linux")]
+
+use std::net;
+
+use regex::Regex;
+
+use regite::{
+    config::{Config, General, Job, Output},
+    Regite,
+};
+
+#[test]
+fn test_date() {
+    let socket = net::UdpSocket::bind("localhost:0").unwrap();
+    let mut regite = Regite::new(Config {
+        general: General {
+            prefix: "prefix".to_string(),
+            hostname: "host".to_string(),
+            graphite_address: socket.local_addr().unwrap().to_string(),
+        },
+        job: vec![Job {
+            interval: 1,
+            command: "/usr/bin/date +%s".to_string(),
+            regex: "(.+)".to_string(),
+            output: vec![Output {
+                name: "date".to_string(),
+                value: "$1".to_string(),
+            }],
+        }],
+    });
+
+    let re = Regex::new("([^ ]+) (\\d+) (\\d+)").unwrap();
+    let mut buf = [0; 100];
+
+    regite.start();
+    for _ in 0..3 {
+        let len = socket.recv(&mut buf).unwrap();
+        let msg = String::from_utf8_lossy(&buf[..len]);
+        let captures = re.captures(&msg).unwrap();
+        assert_eq!("prefix.host.date", &captures[1]);
+        assert_eq!(captures[2], captures[3]);
+    }
+
+    regite.stop();
+    regite.join();
+}
